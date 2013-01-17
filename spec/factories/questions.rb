8ade811c4a8c7b9cd2a939477bb0b3f83356083a
Payton Swick <payton@foolord.com>
2013-01-17 15:10:14 -0500

    Add specs for types of questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,8 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :question do
+  factory :string_question, class: Question do
     label "How are you?"
     field_type "string"
+  end
+
+  factory :boolean_question, class: Question do
+    label "Are you happy?"
+    field_type "boolean"
+  end
+
+  factory :text_question, class: Question do
+    label "Comments?"
+    field_type "text"
   end
 end
--- spec/features/invitation_spec.rb
+++ spec/features/invitation_spec.rb
@@ -8,39 +8,99 @@ describe "An Invitation" do
       @invite1 = FactoryGirl.create :invitation
       @invite1.guests << @guest1 << @guest2
       @invite1.save
-      @question1 = FactoryGirl.create :question
-      @question2 = FactoryGirl.create :question, label: 'Do you like food?'
+      @question1 = FactoryGirl.create :boolean_question
+      @question2 = FactoryGirl.create :string_question
+      @question3 = FactoryGirl.create :text_question
 
       visit invitation_path(@invite1)
     end
 
     context "for the first guest" do
-      it "shows the first question" do
-        within :css, ".guest_#{@guest1.id}" do
-          page.should have_text @question1.label
+      before { @guest_class = ".guest_#{@guest1.id}" }
+
+      context "for the first question" do
+        before { @question_class = ".question_#{@question1.id}" }
+
+        it "shows the question" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_text @question1.label } }
+        end
+
+        it "shows a yes button" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_button 'Yes' } }
+        end
+
+        it "shows a no button" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_button 'No' } }
         end
       end
 
-      it "shows the second question" do
-        within :css, ".guest_#{@guest1.id}" do
-          page.should have_text @question2.label
+      context "for the second question" do
+        before { @question_class = ".question_#{@question2.id}" }
+
+        it "shows the question" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_text @question2.label } }
+        end
+
+        it "shows a text field" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_field "question_#{@question2.id}" } }
+        end
+      end
+
+      context "for the third question" do
+        before { @question_class = ".question_#{@question3.id}" }
+
+        it "shows the question" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_text @question2.label } }
+        end
+
+        it "shows a text area" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_field "question_#{@question3.id}" } }
         end
       end
     end
 
-    context "for the second guest" do
-      it "shows the first question" do
-        within :css, ".guest_#{@guest2.id}" do
-          page.should have_text @question1.label
+     context "for the second guest" do
+      before { @guest_class = ".guest_#{@guest2.id}" }
+
+      context "for the first question" do
+        before { @question_class = ".question_#{@question1.id}" }
+
+        it "shows the question" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_text @question1.label } }
+        end
+
+        it "shows a yes button" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_button 'Yes' } }
+        end
+
+        it "shows a no button" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_button 'No' } }
         end
       end
 
-      it "shows the second question" do
-        within :css, ".guest_#{@guest2.id}" do
-          page.should have_text @question2.label
+      context "for the second question" do
+        before { @question_class = ".question_#{@question2.id}" }
+
+        it "shows the question" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_text @question2.label } }
+        end
+
+        it "shows a text field" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_field "question_#{@question2.id}" } }
         end
       end
-    end
+
+      context "for the third question" do
+        before { @question_class = ".question_#{@question3.id}" }
+
+        it "shows the question" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_text @question2.label } }
+        end
+
+        it "shows a text area" do
+          within(:css, @guest_class) { within(:css, @question_class) { page.should have_field "question_#{@question3.id}" } }
+        end
+      end
+    end   
   end
-
 end
