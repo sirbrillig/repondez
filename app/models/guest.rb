dcd8982ccb8b1e57d12feb33aa4c886a4a971e28
Payton Swick <payton@foolord.com>
2013-01-18 14:44:18 -0500

    Add more admin specs

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,4 +2,8 @@ class Guest < ActiveRecord::Base
   attr_accessible :first_name, :last_name
   belongs_to :invitation
   has_many :answers
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -23,10 +23,6 @@ describe "For an administrator" do
       page.should have_link_to new_guest_url
     end
 
-    it "displays a link to create a new invitation" do
-      page.should have_link_to new_invitation_url
-    end
-
     it "displays a selection for each guest to assign an invitation" do
       page.should have_select "guest_#{@guest1.id}_invitation"
     end
@@ -42,5 +38,62 @@ describe "For an administrator" do
       end
     end
 
+  end
+
+  context "The new guest page" do
+    before do
+      @guest1 = FactoryGirl.create :guest1
+      @guest2 = FactoryGirl.create :guest2
+      @guest3 = FactoryGirl.build :guest3
+      @invite1 = FactoryGirl.create :invitation
+      @invite1.guests << @guest1
+      @old_size = Invitation.size
+      visit new_guest_url
+    end
+
+    it "does show guests with an invitation in the dropdown of other guests" do
+      page.should have_content @guest1.first_name
+    end
+
+    it "does not show guests with no invitation in the dropdown of other guests" do
+      page.should_not have_content @guest2.first_name
+    end
+
+    context "when the form is filled" do
+      before do
+        fill_in 'guest[first_name]', with: @guest3.first_name
+        fill_in 'guest[last_name]', with: @guest3.last_name
+      end
+
+      context "with another guest (invitation) selected" do
+        before do
+          select @guest1.full_name, from: 'guest[invitation]'
+          click_on 'Save'
+        end
+
+        it "does not create a new invitation" do
+          Invitation.size.should eq @old_size
+        end
+
+        it "adds the new guest to the invitation of the other guest" do
+          @guest1.invitation.id.should eq @invite1.id
+        end
+      end
+
+      context "without another invitation selected" do
+        before do
+          select 'None', from: 'guest[invitation]'
+          click_on 'Save'
+        end
+        
+        it "creates a new invitation" do
+          Invitation.size.should eq (@old_size + 1)
+        end
+        
+        it "adds the new guest to the invitation" do
+          @guest1.invitation.id.should eq Invitation.last.id
+        end
+      end
+    end
   end
 end
