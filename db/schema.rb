b897c1ea9511c46a980f089cf27324f341416dab
Payton Swick <payton@foolord.com>
2013-01-20 14:50:59 -0500

    Add email confirmation to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120191741) do
+ActiveRecord::Schema.define(:version => 20130120194627) do
 
   create_table "answers", :force => true do |t|
     t.integer  "guest_id"
@@ -55,8 +55,13 @@ ActiveRecord::Schema.define(:version => 20130120191741) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
