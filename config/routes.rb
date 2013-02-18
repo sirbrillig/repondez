28d674209e908d159018106378d696402d4be73b
Payton Swick <payton@foolord.com>
2013-02-18 17:11:56 -0500

    add admin scope to user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Repondez::Application.routes.draw do
   if Rails.env == 'development'
     devise_for :users
   else
+    # Sign-ups are disabled in production.
     devise_for :users, controllers: { registrations: "registrations" }
   end
 
+  # POST to /tokens.json with email and password to get a token
+  # DELETE to /tokens/[token] to log out.
   resources :tokens, only: [:create, :destroy]
 
-  resources :questions, :users
+  resources :questions
+
+  scope '/admin' do
+    resources :users
+  end
 
   resources :invitations do
     get "find", on: :collection
