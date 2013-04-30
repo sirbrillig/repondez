8f907b9d920e0873d2ae43cc8c30bc883cf74bc6
Payton Swick <payton@foolord.com>
2013-04-30 00:28:11 -0400

    Add basic audits controller

---
+++ app/controllers/audits_controller.rb
@@ -0,0 +1,12 @@
+class AuditsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+    @audits = []
+    Answer.all.each do |answer|
+      @audits.concat(answer.audits)
+    end
+    @audits.sort_by &:created_at
+    @audits.reverse!
+  end
+end
